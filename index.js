2f6ec0bbe5cd5b0691aca1a629bba7b18c815c4f
Youssef233-wq <77164506+Youssef233-wq@users.noreply.github.com>
2023-05-26 20:37:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fs = require('fs'):
 const express = require('express');
+const db = require('./db.json');
 const app = express();
 const PORT = 3000 | process.env.PORT;
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname + '/index.html');
+})
+app.get('/test',(req,res)=>{
+res.end(db.json);
 })
 app.listen(PORT);
