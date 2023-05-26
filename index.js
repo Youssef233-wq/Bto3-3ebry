d60ca46ad01e94c7301bb2c6eea67647f4fce878
Youssef233-wq <77164506+Youssef233-wq@users.noreply.github.com>
2023-05-26 21:10:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require('./db.json');
 const app = express();
 const PORT = 3000 | process.env.PORT;
 app.get('/',(req,res)=>{
-	res.sendFile(__dirname + 'index.html');
+	res.sendFile(__dirname + '/index.html');
 })
 app.get('/test',(req,res)=>{
 res.end(db.json);
