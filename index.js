2499cbfd02323a1b8595a8e2708f4aea4ebde6f9
Youssef233-wq <77164506+Youssef233-wq@users.noreply.github.com>
2023-05-27 10:57:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const express = require('express');
-const db = require('./db.json');
 const app = express();
 const PORT = 3000 | process.env.PORT;
 app.use(express.static());
